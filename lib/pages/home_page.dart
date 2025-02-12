f1b46d99a66517537aa1267781e72dc6efd039a6
Dalai-02 <dalaipacheco3gmail.com>
2025-02-12 11:38:18 -0600

    TabBar Added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:donut_app_2b/utils/my_tab.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,18 +9,66 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //Lista de Tabs
+  List<Widget> myTabs = [
+    //DonutTab
+    MyTab(iconPath: 'lib/icons/donut.png'),
+    //BurguertTab
+    MyTab(iconPath: 'lib/icons/burger.png'),
+    //SmoothieTab
+    MyTab(iconPath: 'lib/icons/smoothie.png'),
+    //PancakeTab
+    MyTab(iconPath: 'lib/icons/pancakes.png'),
+    //PizzatTab
+    MyTab(iconPath: 'lib/icons/pizza.png'),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        // icono izquierdo
-        leading: Icon(
-          Icons.menu,
-          color: Colors.pinkAccent[800],
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          // icono izquierdo
+          leading: Icon(
+            Icons.menu,
+            color: Colors.pinkAccent[800],
+          ),
+          // Icono derecho
+          actions: [Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          )]
         ),
-        // Icono derecho
-        actions: [Icon(Icons.person)]
+        body: Column(
+          children:[
+          //Texto principal
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children: [
+                  Text("I want to ", style: TextStyle(
+                    fontSize: 32 )),
+                  Text("Eat",style: TextStyle(
+                    //Tamaño de letra
+                    fontSize: 32,
+                    //Negritas
+                    fontWeight: FontWeight.bold,
+                    //Subrayado
+                    decoration: TextDecoration.underline,
+                    //Color de letra
+                    color: Colors.pinkAccent[800]  
+                    ) )
+                ],
+              ),
+            ),
+          //TabBar
+          TabBar(tabs: myTabs),
+      
+          //TabBarView
+      
+          //Carrito
+          ] )
       ),
     );
   }
