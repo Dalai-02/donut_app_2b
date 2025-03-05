06ba1b6f4d76ec725c302d8a407ae7e66c6e0aba
Dalai-02 <dalaipacheco3gmail.com>
2025-03-05 11:14:38 -0600

    PriceTag Added

--- lib/tabs/donut_tab.dart
+++ lib/tabs/donut_tab.dart
@@ -22,7 +22,9 @@ class DonutTab extends StatelessWidget {
       //Organiza como distribuir
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount( //
         //Numero de columnas
-        crossAxisCount: 2),
+        crossAxisCount: 2,
+        //Relacion de aspecto (proporción de aspecto)
+        childAspectRatio: 1/1.5),
       itemBuilder: (context, index) {
         return DonutTile(
           donutFlavor: donutsOnSale[index][0],
